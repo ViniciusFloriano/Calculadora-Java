e21307436e61e54fb1489402656dcfa514cc6c9b
ViniciusFloriano <vinicius.dasilvafloriano18@gmail.com>
2025-06-07 11:02:02 -0300

    Criação da tela inicial da calculadora

--- src/Calculos.java
+++ src/Calculos.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculos {
+	
 	public static void main(String[] args) {
 		Calculos c = new Calculos();
 		System.out.println(">>>>"+c.calculadora("5/5"));
